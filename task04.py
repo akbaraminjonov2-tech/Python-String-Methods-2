3d611c8edc709d8384e6f0d5e82b419a277c5bba
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-04 10:35:07 +0500

    task04.py bajarildi

---
+++ task04.py
@@ -0,0 +1,7 @@
+kun = input("Hafta kunini kiriting: ")
+soat = int(input("Dars soatini kiriting: "))
+
+result = "Bugun {} kuni, dars soat {} da.".format(kun, soat)
+
+print(result)
+                                                  
